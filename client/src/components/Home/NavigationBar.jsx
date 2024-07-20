4881c06bf959bc854d3c9491a9c93b8e5d12f56a
furqanistic <furqanistic@gmail.com>
2024-07-21 04:58:44 +0500

    modules-remove

--- client/src/components/Home/NavigationBar.jsx
+++ client/src/components/Home/NavigationBar.jsx
@@ -1,235 +1,86 @@
-"use client"
+import { useState } from 'react'
+import { Avatar, AvatarFallback, AvatarImage } from '../ui/avatar'
 
-import Link from "next/link"
-import * as React from "react"
+const NavigationBar = () => {
+  const [isOpen, setIsOpen] = useState(false)
 
-import { Icons } from "@/components/icons"
-import {
-  NavigationMenu,
-  NavigationMenuContent,
-  NavigationMenuItem,
-  NavigationMenuLink,
-  NavigationMenuList,
-  NavigationMenuTrigger,
-  navigationMenuTriggerStyle,
-} from "@/components/ui/navigation-menu"
-import { cn } from "@/lib/utils"
-
-const components: { title: string; href: string; description: string }[] = [
-  {
-    title: "Alert Dialogs",
-    href: "/docs/primitives/alert-dialog",
-    description:
-      "A modal dialog that interrupts the user with important content and expects a response.",
-  },
-  {
-    title: "Hover Card",
-    href: "/docs/primitives/hover-card",
-    description:
-      "For sighted users to preview content available behind a link.",
-  },
-  {
-    title: "Progress",
-    href: "/docs/primitives/progress",
-    description:
-      "Displays an indicator showing the completion progress of a task, typically displayed as a progress bar.",
-  },
-  {
-    title: "Scroll-area",
-    href: "/docs/primitives/scroll-area",
-    description: "Visually or semantically separates content.",
-  },
-  {
-    title: "Tabs",
-    href: "/docs/primitives/tabs",
-    description:
-      "A set of layered sections of content—known as tab panels—that are displayed one at a time.",
-  },
-  {
-    title: "Tooltip",
-    href: "/docs/primitives/tooltip",
-    description:
-      "A popup that displays information related to an element when the element receives keyboard focus or the mouse hovers over it.",
-  },
-]
-
-export function NavigationMenuDemo() {
   return (
-    <NavigationMenu>
-      <NavigationMenuList>
-        <NavigationMenuItem>
-          <NavigationMenuTrigger>Getting started</NavigationMenuTrigger>
-          <NavigationMenuContent>
-            <ul className="grid gap-3 p-6 md:w-[400px] lg:w-[500px] lg:grid-cols-[.75fr_1fr]">
-              <li className="row-span-3">
-                <NavigationMenuLink asChild>
-                  <a
-                    className="flex h-full w-full select-none flex-col justify-end rounded-md bg-gradient-to-b from-muted/50 to-muted p-6 no-underline outline-none focus:shadow-md"
-                    href="/"
-                  >
-                    <Icons.logo className="h-6 w-6" />
-                    <div className="mb-2 mt-4 text-lg font-medium">
-                      shadcn/ui
-                    </div>
-                    <p className="text-sm leading-tight text-muted-foreground">
-                      Beautifully designed components that you can copy and
-                      paste into your apps. Accessible. Customizable. Open
-                      Source.
-                    </p>
-                  </a>
-                </NavigationMenuLink>
-              </li>
-              <ListItem href="/docs" title="Introduction">
-                Re-usable components built using Radix UI and Tailwind CSS.
-              </ListItem>
-              <ListItem href="/docs/installation" title="Installation">
-                How to install dependencies and structure your app.
-              </ListItem>
-              <ListItem href="/docs/primitives/typography" title="Typography">
-                Styles for headings, paragraphs, lists...etc
-              </ListItem>
-            </ul>
-          </NavigationMenuContent>
-        </NavigationMenuItem>
-        <NavigationMenuItem>
-          <NavigationMenuTrigger>Components</NavigationMenuTrigger>
-          <NavigationMenuContent>
-            <ul className="grid w-[400px] gap-3 p-4 md:w-[500px] md:grid-cols-2 lg:w-[600px] ">
-              {components.map((component) => (
-                <ListItem
-                  key={component.title}
-                  title={component.title}
-                  href={component.href}
-                >
-                  {component.description}
-                </ListItem>
-              ))}
-            </ul>
-          </NavigationMenuContent>
-        </NavigationMenuItem>
-        <NavigationMenuItem>
-          <Link href="/docs" legacyBehavior passHref>
-            <NavigationMenuLink className={navigationMenuTriggerStyle()}>
-              Documentation
-            </NavigationMenuLink>
-          </Link>
-        </NavigationMenuItem>
-      </NavigationMenuList>
-    </NavigationMenu>
+    <header className='bg-slate-950 text-white sticky top-0 w-full z-10'>
+      <div className='flex justify-between items-center p-4'>
+        <div className='text-xl font-bold'>LocalsNearMe</div>
+        <button className='md:hidden p-2' onClick={() => setIsOpen(!isOpen)}>
+          ☰
+        </button>
+        <nav className='hidden md:flex space-x-4'>
+          <ul className='flex space-x-4'>
+            <li>
+              <a
+                href='#'
+                className='py-2 px-4 rounded transition duration-200 hover:bg-slate-800 hover:text-white'
+              >
+                Dashboard
+              </a>
+            </li>
+            <li>
+              <a
+                href='#'
+                className='py-2 px-4 rounded transition duration-200 hover:bg-slate-800 hover:text-white'
+              >
+                Settings
+              </a>
+            </li>
+            <li>
+              <a
+                href='#'
+                className='py-2 px-4 rounded transition duration-200 hover:bg-slate-800 hover:text-white'
+              >
+                Profile
+              </a>
+            </li>
+            <li>
+              <Avatar>
+                <AvatarImage
+                  src='https://github.com/shadcn.png'
+                  alt='@shadcn'
+                />
+                <AvatarFallback>CN</AvatarFallback>
+              </Avatar>
+            </li>
+          </ul>
+        </nav>
+      </div>
+      {isOpen && (
+        <nav className='md:hidden bg-slate-800'>
+          <ul>
+            <li>
+              <a
+                href='#'
+                className='block py-2 px-4 rounded transition duration-200 hover:bg-slate-800 hover:text-white'
+              >
+                Dashboard
+              </a>
+            </li>
+            <li>
+              <a
+                href='#'
+                className='block py-2 px-4 rounded transition duration-200 hover:bg-slate-800 hover:text-white'
+              >
+                Settings
+              </a>
+            </li>
+            <li>
+              <a
+                href='#'
+                className='block py-2 px-4 rounded transition duration-200 hover:bg-slate-800 hover:text-white'
+              >
+                Profile
+              </a>
+            </li>
+          </ul>
+        </nav>
+      )}
+    </header>
   )
 }
 
-const ListItem = React.forwardRef<
-  React.ElementRef<"a">,
-  React.ComponentPropsWithoutRef<"a">
->(({ className, title, children, ...props }, ref) => {
-  return (
-    <li>
-      <NavigationMenuLink asChild>
-        <a
-          ref={ref}
-          className={cn(
-            "block select-none space-y-1 rounded-md p-3 leading-none no-underline outline-none transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground",
-            className
-          )}
-          {...props}
-        >
-          <div className="text-sm font-medium leading-none">{title}</div>
-          <p className="line-clamp-2 text-sm leading-snug text-muted-foreground">
-            {children}
-          </p>
-        </a>
-      </NavigationMenuLink>
-    </li>
-  )
-})
-ListItem.displayName = "ListItem"
-
-
-
-// import { useState } from 'react'
-// import { Avatar, AvatarFallback, AvatarImage } from '../ui/avatar'
-
-// const NavigationBar = () => {
-//   const [isOpen, setIsOpen] = useState(false)
-
-//   return (
-//     <header className='bg-slate-950 text-white sticky top-0 w-full z-10'>
-//       <div className='flex justify-between items-center p-4'>
-//         <div className='text-xl font-bold'>LocalsNearMe</div>
-//         <button className='md:hidden p-2' onClick={() => setIsOpen(!isOpen)}>
-//           ☰
-//         </button>
-//         <nav className='hidden md:flex space-x-4'>
-//           <ul className='flex space-x-4'>
-//             <li>
-//               <a
-//                 href='#'
-//                 className='py-2 px-4 rounded transition duration-200 hover:bg-slate-800 hover:text-white'
-//               >
-//                 Dashboard
-//               </a>
-//             </li>
-//             <li>
-//               <a
-//                 href='#'
-//                 className='py-2 px-4 rounded transition duration-200 hover:bg-slate-800 hover:text-white'
-//               >
-//                 Settings
-//               </a>
-//             </li>
-//             <li>
-//               <a
-//                 href='#'
-//                 className='py-2 px-4 rounded transition duration-200 hover:bg-slate-800 hover:text-white'
-//               >
-//                 Profile
-//               </a>
-//             </li>
-//             <li>
-//               <Avatar>
-//                 <AvatarImage
-//                   src='https://github.com/shadcn.png'
-//                   alt='@shadcn'
-//                 />
-//                 <AvatarFallback>CN</AvatarFallback>
-//               </Avatar>
-//             </li>
-//           </ul>
-//         </nav>
-//       </div>
-//       {isOpen && (
-//         <nav className='md:hidden bg-slate-800'>
-//           <ul>
-//             <li>
-//               <a
-//                 href='#'
-//                 className='block py-2 px-4 rounded transition duration-200 hover:bg-slate-800 hover:text-white'
-//               >
-//                 Dashboard
-//               </a>
-//             </li>
-//             <li>
-//               <a
-//                 href='#'
-//                 className='block py-2 px-4 rounded transition duration-200 hover:bg-slate-800 hover:text-white'
-//               >
-//                 Settings
-//               </a>
-//             </li>
-//             <li>
-//               <a
-//                 href='#'
-//                 className='block py-2 px-4 rounded transition duration-200 hover:bg-slate-800 hover:text-white'
-//               >
-//                 Profile
-//               </a>
-//             </li>
-//           </ul>
-//         </nav>
-//       )}
-//     </header>
-//   )
-// }
-
-// export default NavigationBar
+export default NavigationBar
